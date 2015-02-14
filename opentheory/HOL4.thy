9a6aa63f00ca7f4ef011f037645841355d61ff64
Ramana Kumar <ramana@member.fsf.org>
2015-02-14 07:52:08 +0000

    remove some redundant imports

--- opentheory/HOL4.thy
+++ opentheory/HOL4.thy
@@ -4,23 +4,18 @@ bool-ext { import: bool-int package: bool-ext-1.12 }
 bool-class { import: bool-int import: bool-ext package: bool-class-1.26 }
 himp {
   import: bool-int
-  import: bool-ext
   import: bool-class
   article: "HOL4Imp.art"
 }
 hbool {
   import: bool-int
-  import: bool-ext
-  import: bool-class
   import: himp
   article: "HOL4Bool.art"
 }
 hsat {
   import: bool-int
-  import: bool-ext
   import: bool-class
   import: hbool
-  import: himp
   article: "HOL4Sat.art"
 }
 main {
